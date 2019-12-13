f3da4fb8f798bd32a4f3aedf1727c5460c2fe045
zul <fahrizalmursalin06@gmail.com>
2019-12-13 22:42:51 +0700

    feat: creating detail layout.

---
+++ app/src/main/java/com/id/zul/foodie/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.id.zul.foodie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
